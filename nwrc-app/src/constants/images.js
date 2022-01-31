0081b2e7b5053a76ed03da237301db524159bb76
Tessa Hayden <tessahayden@Tessas-MacBook-Air.local>
2022-01-30 21:56:20 -0800

    Data and Image Modules

---
+++ nwrc-app/src/constants/images.js
@@ -0,0 +1,113 @@
+import defuegoRestaurant from '../assets/defuegocarousel.jpg';
+import defuegoRestaurantWhiskey from '../assets/defuegowhiskey.jpg';
+import defuegofRestaurantPool from '../assets/defuegopool.jpg';
+import defuegoFood from '../assets/defuegofoodoffering.png';
+import defuegofFoodLunch from '../assets/defuegolunchspread.jpg';
+import defuegoBrunch from '../assets/defuegobrunchfood.png';
+import defuegoFoodSalmon from '../assets/defuegosalmonbowl.jpg';
+import defuegoDrink from '../assets/defuegowhiskeypour.jpeg';
+import haydensRestaurant from '../assets/Haydenscarousel.jpg';
+import haydensRestaurantLake from '../assets/HaydenslakeSz.jpg';
+import haydensRestaurantLittle from '../assets/Haydenslittle.jpg';
+import haydensFoodBeef from '../assets/haydenbeefplate.jpg';
+import haydensFoodSandwich from '../assets/haydensandwich.jpeg';
+import haydensFoodSalad from '../assets/haydenssalad.jpeg';
+import haydensFoodSalmon from '../assets/haydensSalmonfood.jpeg';
+import lechonRestaurant from '../assets/lechondiningroom.jpg';
+import lechonRestaurantOutside from '../assets/lechonoutside.jpg';
+import lechonFoodBurger from '../assets/lechonburger.jpg';
+import lechonFoodFish from '../assets/lechonfishplate.jpg'
+import lechonFood from '../assets/lechonfood.jpg';
+import lechonFoodOutside from '../assets/lechonfoodoutside.jpg';
+import shigezoRestaurantOutside from '../assets/shigezooutside.png';
+import shigezoRestaurantInside from '../assets/shigezointerior.jpg';
+import shigezoRestaurant from '../assets/ShigezoSz.jpg';
+import shigezoFoodSushi from '../assets/shigezocarousel1.jpg';
+import shigezoFoodBeef from '../assets/shigezocarousel2.jpg';
+import shigezoFoodIzakaya from '../assets/shigezocarousel3.jpg';
+import shigezoFoodGyoza from '../assets/shigezocarousel4.jpg';
+import shigezoFood from '../assets/shigezofood1.jpg';
+import shigezoMenu from '../assets/ShigezoMenu.png';
+import southparkRestaurantBar from '../assets/southparkbar.jpeg';
+import southparkRestaurantBarBottles from '../assets/southparkmedium.jpeg';
+import southparkRestaurant from "../assets/southparkSz.jpeg";
+import southparkRestaurantSeafood from "../assets/southparkseafoodcounter.jpeg";
+import southparkFood from "../assets/Southparkplate.jpeg";
+import southparkFoodSpread from "../assets/southparkseafoodspread.jpeg";
+import southparkFoodStew from "../assets/southparkseafoodstew.jpeg";
+import southparkDrink from "../assets/southparkdrink.jpeg";
+import stonecliffRestaurant from "../assets/stonecliffSz.jpg";
+import stonecliffRestaurantPatio from "../assets/stonecliffback.jpg";
+import stonecliffRestaurantBar from "../assets/stonecliffbar.jpg";
+import stonecliffRestaurantDiners from "../assets/StoneCliff395.jpg";
+import stonecliffRestaurantUpstairs from "../assets/stonecliffupstairs.jpg";
+import stonecliffFoodFish from "../assets/Stone_cliff_fishfood.jpg";
+import stonecliffFoodPlate from "../assets/Stone_Cliff_foodplate.jpg";
+import stonecliffFoodDish from "../assets/StonecliffFood1.jpg";
+import stonecliffFoodPasta from "../assets/stonecliffFood2.jpg";
+import jellyfish from "../assets/jelly.jpeg";
+import jellyfishBar from "../assets/jellyfish.jpg";
+import ernestosEvent from "../assets/Ernestosevent.jpg";
+import wineryEvent from "../assets/eolaevent.jpg";
+import champagne from "../assets/eoladrinks.jpg";
+import outsideEvent from "../assets/stonecliffevent.jpg";
+import peopleDining from "../assets/southparkpeopledining.jpeg";
+import nwrcLogo from '../assets/nwreslogo.svg';
+
+export default {
+    defuegoRestaurant,
+    defuegoRestaurantWhiskey,
+    defuegofRestaurantPool,
+    defuegoFood,
+    defuegofFoodLunch,
+    defuegoBrunch,
+    defuegoFoodSalmon,
+    defuegoDrink,
+    haydensRestaurant,
+    haydensRestaurantLake,
+    haydensRestaurantLittle,
+    haydensFoodBeef,
+    haydensFoodSandwich,
+    haydensFoodSalad,
+    haydensFoodSalmon,
+    lechonRestaurant,
+    lechonRestaurantOutside,
+    lechonFoodBurger,
+    lechonFoodFish,
+    lechonFood,
+    lechonFoodOutside,
+    shigezoRestaurantOutside,
+    shigezoRestaurantInside,
+    shigezoRestaurant,
+    shigezoFoodSushi,
+    shigezoFoodBeef,
+    shigezoFoodIzakaya,
+    shigezoFoodGyoza,
+    shigezoFood,
+    shigezoMenu,
+    southparkRestaurantBar,
+    southparkRestaurantBarBottles,
+    southparkRestaurant,
+    southparkRestaurantSeafood,
+    southparkFood,
+    southparkFoodSpread,
+    southparkFoodStew,
+    southparkDrink,
+    stonecliffRestaurant,
+    stonecliffRestaurantPatio,
+    stonecliffRestaurantBar,
+    stonecliffRestaurantDiners,
+    stonecliffRestaurantUpstairs,
+    stonecliffFoodFish,
+    stonecliffFoodPlate,
+    stonecliffFoodDish,
+    stonecliffFoodPasta,
+    jellyfish,
+    jellyfishBar,
+    ernestosEvent,
+    wineryEvent,
+    champagne,
+    outsideEvent,
+    peopleDining,
+    nwrcLogo
+};
