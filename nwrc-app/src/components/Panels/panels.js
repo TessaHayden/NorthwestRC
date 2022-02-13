009ad5db65546b3537577e1c0233b8f7c5fb552b
Tessa Hayden <tessahayden@Tessas-MacBook-Air.local>
2022-02-13 09:05:15 -0800

    Panel Animation Working needs debugging

--- nwrc-app/src/components/Panels/panels.js
+++ nwrc-app/src/components/Panels/panels.js
@@ -1,19 +1,14 @@
 import React from "react";
 
-const Panels = ({ name, description, website}) => (
-  
-    <div >
-      <div>
-        <p>{name}</p>
-      </div>
-      <div>
-        <p>{description}</p>
-      </div>
-      <div>
-        <a href={website}><p>Visit {name}</p></a>
-      </div>
-    </div>
- 
-);
+const Panels = ({ name, description, website }) => {
+    return (
+  <div className="panel_text">
+    <p>{description}</p>
+    <p>{name}</p>
+    <a href={website}>
+      <p>Visit {name}</p>
+    </a>
+  </div>
+)};
 
 export default Panels;
--- nwrc-app/src/container/Gallery/gallery.js
+++ nwrc-app/src/container/Gallery/gallery.js
@@ -1,26 +1,30 @@
 import React, { useState } from "react";
-
-import { Panels } from "../../components/index";
 import { data } from "../../constants/index";
 import "./gallery.css";
 
 const Gallery = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleOpen = () => {
-    setIsOpen(!isOpen);
-  };
-    
   return (
-    <div className='panels' onClick={toggleOpen}>
-      {data.restaurants.map((restaurant, index) => (
-        <div className={`panel panel${index} ${isOpen ? 'open open-active' : ''}`}>
-          <Panels
-            key={index}
-            name={restaurant.name}
-            description={restaurant.description}
-            website={restaurant.website}
-          />
+    <div className="panels">
+      {data.restaurants.map((restaurants, index) => (
+        <div
+          className={`panel panel${index} ${
+            isOpen === index ? "open open-active" : ""
+          }`}
+          onClick={() => setIsOpen(index)}
+        >
+          <div>
+            <p>{restaurants.description}</p>
+          </div>
+          <div>
+            <p>{restaurants.name}</p>
+          </div>
+          <div>
+            <p>
+              Visit<a href={restaurants.website}>{restaurants.name}</a>
+            </p>
+          </div>
         </div>
       ))}
     </div>
